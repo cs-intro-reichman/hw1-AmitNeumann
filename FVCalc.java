ae2858cd2428c1ad3dc6f8b83b3e0be1dce70533
Amit Neumann <amit.neumann@post.runi.ac.il>
2024-11-07 18:08:26 +0200

    FVC by amit neu done

--- FVCalc.java
+++ FVCalc.java
@@ -3,10 +3,10 @@ public class FVCalc {
 	public static void main(String[] args){
 		int currentValue=Integer.parseInt(args[0]), rate=Integer.parseInt(args[1]),
 		n=Integer.parseInt(args[2]);
-		double dRate=(double)(rate);//from int to double 10.0
-		double base=1+dRate/100; //double base for mathpow+adjust 
+		double base=1+(rate/100.0); //double base for mathpow+adjust 
 		double exponent=(double)(n);//from int to double
 		double futureValue=(currentValue*(Math.pow(base,exponent))); //return f value in double
-		System.out.println("After "+n+" years, $"+currentValue+" saved at "+dRate+"% will yield $"+(int)(futureValue));
+		double dRate=(double)(rate);//from int to double 10.0 for printing
+		System.out.println("After "+n+" years, a $"+currentValue+" saved at "+dRate+"% will yield $"+(int)(futureValue));
 	}
 }
